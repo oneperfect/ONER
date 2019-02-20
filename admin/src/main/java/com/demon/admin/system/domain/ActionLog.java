2e5711e9f84c157b8ddf78813a75fa885af63abd
oneperfect <1501666987@qq.com>
2019-02-20 23:55:08 +0800

    添加实体类数据

--- admin/src/main/java/com/demon/admin/system/domain/ActionLog.java
+++ admin/src/main/java/com/demon/admin/system/domain/ActionLog.java
@@ -1,29 +1,50 @@
 package com.demon.admin.system.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table
+@Table(name = "sys_action_log")
 @Data
 @EntityListeners(AuditingEntityListener.class)
 public class ActionLog {
 
+    // 日志id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    // 日志名称
     private String name;
+    // 日志类型
     private Byte type;
+    // 日志操作ip地址
     private String ipaddr;
-    private String  clazz;
+    // 产生日志的类
+    private String clazz;
+    // 产生日志的方法
     private String method;
+    // 产生日志的表
     private String model;
+    // 产生日志的用户
     private Long recordId;
+    // 日志消息
+    @Lob
+    @Column(columnDefinition = "TEXT")
     private String message;
+    // 产生日志的时间
+    @CreatedDate
     private Date createDate;
+    // 产生日志的用户
+    @CreatedBy
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    @JsonIgnore
     private User createBy;
 
     public ActionLog() {
--- admin/src/main/java/com/demon/admin/system/domain/User.java
+++ admin/src/main/java/com/demon/admin/system/domain/User.java
@@ -1,9 +1,20 @@
 package com.demon.admin.system.domain;
 
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "sys_user")
+@Data
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String nickname;
@@ -16,6 +27,7 @@ public class User {
     private String phone;
     private Long isRole;
     private String remake;
+    @CreatedDate
     private Date createDate;
     private Date updateDate;
     private Long status;
