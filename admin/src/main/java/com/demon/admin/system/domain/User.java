937b004b97ba2545a66f369d1e0373b95f8ba936
oneperfect <1501666987@qq.com>
2019-02-20 18:33:09 +0800

    添加实体类数据

--- admin/src/main/java/com/demon/admin/system/domain/User.java
+++ admin/src/main/java/com/demon/admin/system/domain/User.java
@@ -1,7 +1,25 @@
 package com.demon.admin.system.domain;
 
+import java.util.Date;
+
 public class User {
 
     private Long id;
+    private String username;
+    private String nickname;
+    private String password;
+    private String salt;
+    private Long deptId;
+    private String picture;
+    private String sex;
+    private String email;
+    private String phone;
+    private Long isRole;
+    private String remake;
+    private Date createDate;
+    private Date updateDate;
+    private Long status;
 
+    public User() {
+    }
 }
